46ff613b74ca64bcf5f1b1915d72bd9353d1c60c
Lukas Scholz <lukas.scholz@deutschebahn.com>
2020-03-10 20:57:15 +0100

    added urlib

--- WebCrawler.py
+++ WebCrawler.py
@@ -1,13 +1,7 @@
-import requests
+import urllib.request
+from bs4 import BeautifulSoup
 
-### Functions ###
+url = 'https://www.mz-web.de/'
 
-def status(status_code):
-    if requests.status_codes != 200:
-        print("The Website isn't available at the moment")
-        return False
+page = urllib.request.urlopen(url)
 
-
-### Functions ###
-
-status()
